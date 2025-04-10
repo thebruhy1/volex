12ed4c4332cadc22a43a0ccacd3008ad41bab8d8
thebruhy <thebruhy1@gmail.com>
2025-04-10 21:07:48 +0300

    Add Google AdSense verification code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7119422349393714" crossOrigin="anonymous"></script>
+      </head>
       <body className={inter.className}>
         <main className="min-h-screen">
           {children}
